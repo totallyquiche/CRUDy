aa94a288bf72829300e18c620658a94293e5d8b0
Brian N. Dady <bndady@gmail.com>
2020-12-26 16:22:04 -0600

    Refactors reflection logic and tests Config::get()

--- App/Tests/ConfigTest.php
+++ App/Tests/ConfigTest.php
@@ -3,7 +3,7 @@
 namespace App\Tests;
 
 use App\Config;
-use \ReflectionObject;
+use \ReflectionClass;
 
 class ConfigTest extends BaseTest
 {
@@ -16,12 +16,9 @@ class ConfigTest extends BaseTest
     {
         $config = new Config;
 
-        // Clear existing config options
-        $reflection = new ReflectionObject($config);
-        $config_options_property = $reflection->getProperty('config_options');
-        $config_options_property->setAccessible(true);
-        $config_options = $config_options_property->setValue([]);
-        $config_options_property->setAccessible(false);
+        $original_config_options = $this->getConfigOptions();
+
+        $this->setConfigOptions([]);
 
         $config_options = [
             'DB_NAME' => 'my_db',
@@ -43,12 +40,7 @@ class ConfigTest extends BaseTest
 
         unlink($file_name);
 
-        // Get config options
-        $reflection = new ReflectionObject($config);
-        $config_options_property = $reflection->getProperty('config_options');
-        $config_options_property->setAccessible(true);
-        $actual_config_options = $config_options_property->getValue();
-        $config_options_property->setAccessible(false);
+
 
         $expected_config_options = [];
 
@@ -56,6 +48,79 @@ class ConfigTest extends BaseTest
             $expected_config_options[trim($key)] = trim($value);
         }
 
+        $actual_config_options = $this->getConfigOptions();
+
+        $this->setConfigOptions($original_config_options);
+
         return $expected_config_options === $actual_config_options;
+    }
+
+    /**
+     * Test that the get method returns the expected value.
+     *
+     * @return bool
+     */
+    public function test_get_returns_expected_value() : bool
+    {
+        $config = new Config;
+
+        $original_config_options = $this->getConfigOptions();
+
+        $this->setConfigOptions([]);
+
+        $config_options = [
+            'CONFIG_NAME' => 'config_value'
+        ];
+
+        $expected_config_file_contents = '';
+
+        foreach ($config_options as $key => $value) {
+            $expected_config_file_contents .= $key . '=' . $value . PHP_EOL;
+        }
+
+        $file_name = __DIR__ . '/.env.' . microtime(true);
+        $file_handler = file_put_contents($file_name, $expected_config_file_contents);
+
+        $config->load($file_name);
+
+        unlink($file_name);
+
+        $expected_config_value = Config::get('CONFIG_NAME');
+
+        $this->setConfigOptions($original_config_options);
+
+        return $expected_config_value == $config_options['CONFIG_NAME'];
+    }
+
+    /**
+     * Clears the current config options.
+     *
+     * @return array
+     */
+    private function getConfigOptions() : array
+    {
+        $reflection = new ReflectionClass(Config::class);
+        $config_options_property = $reflection->getProperty('config_options');
+        $config_options_property->setAccessible(true);
+        $config_options = $config_options_property->getValue();
+        $config_options_property->setAccessible(false);
+
+        return $config_options;
+    }
+
+    /**
+     * Updates the current config options.
+     *
+     * @param array $value
+     *
+     * @return void
+     */
+    private function setConfigOptions(array $value) : void
+    {
+        $reflection = new ReflectionClass(Config::class);
+        $config_options_property = $reflection->getProperty('config_options');
+        $config_options_property->setAccessible(true);
+        $config_options = $config_options_property->setValue($value);
+        $config_options_property->setAccessible(false);
     }
 }
