c565659a6d2f53df3fafcb6556a5adff11d0eead
Brian N. Dady <bndady@gmail.com>
2020-12-26 15:24:18 -0600

    Refactors index into bootstrap and index

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php declare(strict_types=1);
 
-require_once(__DIR__ . '/vendor/autoload.php');
-
-use App\Config;
 use App\Router;
 
-Config::load('.env');
+require_once(__DIR__ . '/bootstrap.php');
 
 (function () {
     $router = new Router;
