700dfb5da0c3a908eef154f22ff9a7eeab64c2d2
Brian N. Dady <bndady@gmail.com>
2020-12-27 11:51:24 -0600

    Allows specifying connection info when getting instance of DB adapter

--- App/Database/DatabaseAdapterInterface.php
+++ App/Database/DatabaseAdapterInterface.php
@@ -7,9 +7,14 @@ interface DatabaseAdapterInterface
     /**
      * Singleton to ensure we always use the same instance of this interface.
      *
+     * @param string|null $db_host
+     * @param string|null $db_name
+     * @param string|null $db_user
+     * @param string|null $db_password
+     *
      * @return DatabaseAdapterInterface
      */
-    public static function getInstance() : DatabaseAdapterInterface;
+    public static function getInstance(?string $db_host = null, ?string $db_name = null, ?string $db_user = null, ?string $db_password = null) : DatabaseAdapterInterface;
 
     /**
      * Run a query and get the results.
