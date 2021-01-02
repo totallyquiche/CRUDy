1cb0f09556241f196743d4d208f2f84e0974d410
Brian N. Dady <bndady@gmail.com>
2021-01-02 14:01:56 -0600

    Minor spacing fix

--- App/Database/PdoAdapter.php
+++ App/Database/PdoAdapter.php
@@ -37,9 +37,9 @@ class PdoAdapter implements DatabaseAdapterInterface
         $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8mb4";
 
         $options = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false
+            PDO::ATTR_EMULATE_PREPARES => false
         ];
 
         try {
