3fb51282b7538559786610edebc4820c8ea28ae1
Brian N. Dady <bndady@gmail.com>
2021-01-02 14:20:29 -0600

    Adds query() test to PdoAdapterTest

--- App/Tests/PdoAdapterTest.php
+++ App/Tests/PdoAdapterTest.php
@@ -68,4 +68,36 @@ class PdoAdapterTest extends BaseTest
 
         return $second_instance === $third_instance;
     }
+
+    /**
+     * Test that query() returns the results of a query.
+     *
+     * @return bool
+     */
+    public function test_query_returns_query_results()
+    {
+        $this->database_adapter = PdoAdapter::getInstance(
+            Config::get('TEST_DB_HOST'),
+            Config::get('TEST_DB_NAME'),
+            Config::get('TEST_DB_USER'),
+            Config::get('TEST_DB_PASSWORD')
+        );
+
+        $table_name = 'pdo_adapter_test_' . str_replace('.', '_', microtime(true));
+        $column_name = 'test_name';
+        $row_value = 'test value';
+
+        $this->database_adapter->execute("CREATE TABLE `$table_name` (`$column_name` VARCHAR(10) NOT NULL)");
+        $this->database_adapter->execute("INSERT INTO `$table_name` (`$column_name`) VALUES ('$row_value')");
+
+        $query_results = $this->database_adapter->query("SELECT * FROM `$table_name`");
+
+        $this->database_adapter->execute("DROP TABLE `$table_name`");
+
+        return
+            (count($query_results) === 1) &&
+            (count($query_results[0]) === 1) &&
+            isset($query_results[0][$column_name]) &&
+            $query_results[0][$column_name] === $row_value;
+    }
 }
