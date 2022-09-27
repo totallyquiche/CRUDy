1c101c18e9accb8d201df3a640f1478f195235b8
Brian Dady <bndady@gmail.com>
2022-09-26 22:49:54 -0500

    Updates 404 page

--- App/Views/404.php
+++ App/Views/404.php
@@ -2,4 +2,8 @@
 <header>
     <h1>Oh, 💩!</h1>
 </header>
-<main></main>
+<main>
+    <span style="display: inline-block; width: 100%; text-align: center;">
+        Sorry, but the requested page could not be found.
+    </span>
+</main>
