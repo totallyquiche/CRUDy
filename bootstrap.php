f11ce9683aa3996a17acec6c79bdf3da0e07e1e6
Brian Dady <bndady@gmail.com>
2022-09-26 22:57:23 -0500

    Makes namespaced method call more succinct

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 require_once(__DIR__ . '/autoload.php');
 
-use App\Config;
-
-Config::load(__DIR__ . '/.env');
+App\Config::load(__DIR__ . '/.env');
