1102c252361625656889d5e5421e26cacc8d5847
Brian Dady <bndady@gmail.com>
2022-10-10 16:18:46 -0500

    Removes stylesheet in favor of in-line styles

--- App/View/Templates/page.php
+++ App/View/Templates/page.php
@@ -6,7 +6,32 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $site_title ?></title>
     <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💩</text></svg>">
-    <link rel="stylesheet" href="<?= $site_url ?>/App/Assets/style.css">
+    <style>
+        body {
+            margin: 0;
+            background: #8080FF;
+            font-family: sans-serif;
+        }
+
+        header {
+            padding: 1em 0 0 1em;
+            min-height: 150px;
+            background:#f8f8f8;
+        }
+
+        h1 {
+            color: #8080FF;
+            font-size: 5em;
+            margin-bottom: 0;
+            line-height: 1em;
+        }
+
+        main {
+            width: calc(100% - 2em);
+            margin: 1em;
+            color: #f8f8f8;
+        }
+    </style>
 </head>
 <body>
     {{ 💩 }}
